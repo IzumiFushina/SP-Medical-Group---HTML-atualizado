0eecc9df4fa35f90a072ef44d62dc2bc3d27a471
GuilhermeVeigaPedromilo <guipedromilo@gmail.com>
2023-09-29 14:43:28 -0300

    Update

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 8019; // Porta em que o servidor será executado
+// Rota para exibir a página index.html
+app.get('/', (req, res) => {
+res.sendFile(__dirname + '/home.html');
+});
+
+app.use(express.static(__dirname + '/'));
+
+// Iniciar o servidor
+app.listen(port, () => {
+console.log(`Servidor Express está rodando na porta ${port}`);
+});
